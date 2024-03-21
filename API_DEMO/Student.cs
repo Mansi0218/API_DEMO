743d74020030c178557961b586c945a3bd614a08
Mansi0218 <95520980+Mansi0218@users.noreply.github.com>
2024-03-21 13:33:11 +0530

    Add files via upload

---
+++ API_DEMO/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace API_DEMO
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int MobNo { get; set; }
+    }
+}
